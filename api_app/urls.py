152807bb9e28ad1d0e3bf72a24450256ce471214
Sam Ngigi <sam.t.ngigi@gmail.com>
2018-03-23 06:02:10 +0300

    What is causing this app error on deployment

--- api_app/urls.py
+++ api_app/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^api/$', views.TxtList.as_view()),
-    url(r'^api/(?P<pk>[0-9]+)/$', views.TxtDetail.as_view()),
+    url(r'^$', views.TxtList.as_view()),
+    url(r'^(?P<pk>[0-9]+)/$', views.TxtDetail.as_view()),
 ]
 
 if settings.DEBUG:
